fa252a92455a8cbc0e28ebba66fd725b0d09016d
Michael Shimkus <m.shimkus@f5.com>
2020-04-15 15:01:53 -0700

    updates

--- HttpTriggerRevoke/__init__.py
+++ HttpTriggerRevoke/__init__.py
@@ -4,6 +4,7 @@ import json
 
 # pylint: disable=import-error
 # pylint: disable=no-name-in-module
+# pylint: disable=unused-variable
 import azure.functions as func
 from f5sdk.bigiq import ManagementClient
 from f5sdk.bigiq.licensing import AssignmentClient
@@ -21,9 +22,11 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
             status_code=400
         )
 
+    logging.info('Request body: ' + json.dumps(req_body))
+
     operation = req_body['operation']
-    group = req_body['context']['resourceGroupName']) 
-    resource = req_body['context']['resourceName'])
+    group = req_body['context']['resourceGroupName']
+    resource = req_body['context']['resourceName']
     bigiq_address = req_body['properties']['bigIqAddress']
     bigiq_username = req_body['properties']['bigIqUsername'] 
     key_vault_name = req_body['properties']['keyVaultName'] 
@@ -31,10 +34,29 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     bigiq_license_sku = req_body['properties']['bigIqLicenseSkuKeyword1'] 
     bigiq_license_unit = req_body['properties']['bigIqLicenseUnitOfMeasure']
 
-    logging.info('Python HTTP trigger function processed a request: ' + operation + ' ' + group + ' ' + resource)  
+    logging.info('BIG-IQ password: ' + os.environ['BIGIQ_PASSWORD'])
+
+    # get vmss instance ip addresses
+
+    # get license bigiq assignments
+
+    # revoke any licenses for devices that are not in creating or running state
+  
+    response = {
+        'operation': operation,
+        'resourceGroup': group,
+        'resourceName': resource,
+        'bigiqAddress': bigiq_address,
+        'bigiqUsername': bigiq_username,
+        'bigiqLicensePool': bigiq_license_pool,
+        'bigiqLicenseSku': bigiq_license_sku,
+        'bigiqLicenseUnit': bigiq_license_unit,
+        'keyVault': key_vault_name,
+        'bigiqPassword': os.environ['BIGIQ_PASSWORD']
+    }
 
     if operation:
-        return func.HttpResponse(f"Hello {operation}!")
+        return func.HttpResponse(json.dumps(response))
     else:
         return func.HttpResponse(
              "Please pass an operation on the query string or in the request body",
